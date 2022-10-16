5f03cb9a508d792739019c5502d0bb6aadeb3af6
Viphakone Monty <10835390@my.uvu.edu>
2022-10-16 11:35:17 -0600

    added/modified setting.py with new  DATABASE info

--- restfulapicrud/restfulapicrud/settings.py
+++ restfulapicrud/restfulapicrud/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'restfulapicrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'resfulapi_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '172.18.0.3',
+        'PORT': '5432',
     }
 }
 
